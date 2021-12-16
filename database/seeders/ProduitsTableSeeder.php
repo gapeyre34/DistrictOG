4761f8b3d296a9b725d2fca2acc3da86f3ed2149
gabriel <g.peyre@yahoo.com>
2021-12-16 21:44:42 +0100

    Seeders de la table produits (ajout de 9 produits)

---
+++ database/seeders/ProduitsTableSeeder.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Produit;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+
+class ProduitsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $produit = new Produit();
+        $produit->nom = "Pull noir en laine";
+        $produit->prix_ht = 25;
+        $produit->description = "Un pull confortable en laine";
+        $produit->photo_principale = "";
+        $produit->save();
+
+        $produit = new Produit();
+        $produit->nom = "Pull blanc en laine";
+        $produit->prix_ht = 25;
+        $produit->description = "Un pull confortable en laine";
+        $produit->photo_principale = "";
+        $produit->save();
+
+        $produit = new Produit();
+        $produit->nom = "Pull bleu en laine";
+        $produit->prix_ht = 25;
+        $produit->description = "Un pull confortable en laine";
+        $produit->photo_principale = "";
+        $produit->save();
+
+        $produit = new Produit();
+        $produit->nom = "T-shirt SkateMafia";
+        $produit->prix_ht = 18;
+        $produit->description = "Un T-shirt bien stylé pour l'été";
+        $produit->photo_principale = "";
+        $produit->save();
+
+        $produit = new Produit();
+        $produit->nom = "Doudoune The North Face";
+        $produit->prix_ht = 175;
+        $produit->description = "Une doudoune parfaite pour les plus frileux d'entre vous";
+        $produit->photo_principale = "";
+        $produit->save();
+
+        $produit = new Produit();
+        $produit->nom = "Sneakers Air max 97";
+        $produit->prix_ht = 90;
+        $produit->description = "Habilles bien tes chaussures pour un max de confort";
+        $produit->photo_principale = "";
+        $produit->save();
+
+        $produit = new Produit();
+        $produit->nom = "Veste Jacker";
+        $produit->prix_ht = 68;
+        $produit->description = "Une veste pour les skateurs de la street";
+        $produit->photo_principale = "";
+        $produit->save();
+
+        $produit = new Produit();
+        $produit->nom = "T-shirt Tégrité";
+        $produit->prix_ht = 11;
+        $produit->description = "Pour les grands fans de South Park";
+        $produit->photo_principale = "";
+        $produit->save();
+
+        $produit = new Produit();
+        $produit->nom = "Chaussettes Mickael Jackson";
+        $produit->prix_ht = 2;
+        $produit->description = "Décidément il y en a pour tous les gouts et a ce prix la c'est cadeau";
+        $produit->photo_principale = "";
+        $produit->save();
+    }
+}
